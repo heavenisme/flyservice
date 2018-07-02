6b8480c2002fb817eaae1dd1053b422ade4170ea
heavenisme <heavenisme@aliyun.com>
2018-07-02 11:49:36 +0800

    多数据源配置

--- src/main/java/com/heaven/fly/controller/UserInfoController.java
+++ src/main/java/com/heaven/fly/controller/UserInfoController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +29,7 @@ import java.util.List;
 @RequestMapping("userInfo")
 @Api(tags = {"用户操作接口"}, description = "userInfoControler")
 public class UserInfoController {
-    @Resource
+    @Autowired
     private UserInfoService userInfoService;
 
     @PostMapping("/hello")
